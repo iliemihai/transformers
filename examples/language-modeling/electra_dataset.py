a2c2fb85d84e234543accb2df58fb86e9ee9e0b1
iliemihai <ily_sin_mike@yahoo.com>
2020-06-06 23:52:55 +0300

    Added new params

--- examples/language-modeling/electra_dataset.py
+++ examples/language-modeling/electra_dataset.py
@@ -11,27 +11,21 @@ from transformers import (
     PreTrainedTokenizer,
 )
 from electra_tokenizer import ElectROTokenizer
-#from transformers.tokenization_electra import ElectraTokenizer
 
 logger = logging.getLogger(__name__)
 
 class LineByLineTextDataset(Dataset):
     def __init__(self, tokenizer: PreTrainedTokenizer, file_path: str, block_size=512, local_rank=-1):
         assert os.path.isfile(file_path)
-        logger.info("Creating features from dataset file at %s", file_path)
         
-        # -------------------------- TAKING LONG TIME, NEED TO DO IT PARALLEL ON BATCHES
-
         logger.info("Reading file %s", file_path)
 
         with open(file_path, encoding="utf-8") as f:
             lines = [line for line in f.read().splitlines() if (len(line) > 0 and not line.isspace())]
 
         logger.info("Running tokenization")
-        self.examples = tokenizer._tokenizer.encode_batch(lines)#[tokenizer.encode(line) for line in lines]
+        self.examples = tokenizer._tokenizer.encode_batch(lines)
         
-        # -------------------------- CHANGES END
-
     def __len__(self):
         return len(self.examples)
 
